691e46727b83b04b3eb9b043b6320e980ef38573
Rohinthrathna652 <rohinthrathna652@gmail.com>
2025-09-13 14:08:02 +0530

    feat: add update-product and delete-product

--- stock_manager/lib/providers/product_provider.dart
+++ stock_manager/lib/providers/product_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import '../models/product.dart';
 import '../database/db_helper.dart';
 
@@ -6,13 +6,27 @@ class ProductProvider with ChangeNotifier {
   List<Product> _products = [];
   List<Product> get products => _products;
 
-  Future<void> fetchProducts() async {
-    _products = await DBHelper.instance.getProducts();
+  // Load all products
+  Future<void> loadProducts() async {
+    _products = await DBHelper.getProducts();
     notifyListeners();
   }
 
+  // Add new product
   Future<void> addProduct(Product product) async {
-    await DBHelper.instance.insertProduct(product);
-    await fetchProducts();
+    await DBHelper.insertProduct(product);
+    await loadProducts();
+  }
+
+  // Update existing product
+  Future<void> updateProduct(Product product) async {
+    await DBHelper.updateProduct(product);
+    await loadProducts();
+  }
+
+  // Delete product by ID
+  Future<void> deleteProduct(int id) async {
+    await DBHelper.deleteProduct(id);
+    await loadProducts();
   }
 }
